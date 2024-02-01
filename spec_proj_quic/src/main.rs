8fdbc2ce49fab2a069b3f2857b4ea64ea49b703e
YutongDu <yutong.du@aalto.fi>
2024-02-01 13:55:17 +0200

    发送了四个字节的数据

--- spec_proj_quic/src/main.rs
+++ spec_proj_quic/src/main.rs
@@ -4,12 +4,22 @@ use std::{error::Error};
 // mod common;
 mod common_util;
 mod insec_conn;
+mod unidirec_streams;
+
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn Error>> {
+//     // server and client are running on the same thread asynchronously
+//     let addr = "127.0.0.1:5000".parse().unwrap();
+//     tokio::spawn(insec_conn::run_server(addr));
+//     insec_conn::run_client(addr).await?;
+//     Ok(())
+// }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // server and client are running on the same thread asynchronously
     let addr = "127.0.0.1:5000".parse().unwrap();
-    tokio::spawn(insec_conn::run_server(addr));
-    insec_conn::run_client(addr).await?;
+    tokio::spawn(unidirec_streams::run_server(addr));
+    unidirec_streams::run_client(addr).await?;
     Ok(())
 }
---
+++ spec_proj_quic/src/unidirec_streams.rs
@@ -0,0 +1,121 @@
+#![cfg(feature = "rustls")]
+use std::{error::Error, net::SocketAddr, sync::Arc};
+use anyhow;
+use quinn::{ClientConfig, Connection, Endpoint};
+
+// mod common;
+use crate::common_util;
+
+/// Runs a QUIC server bound to given address.
+pub(crate) async fn run_server(addr: SocketAddr) -> anyhow::Result<()>{
+    let (endpoint, _server_cert) = common_util::common_util::make_server_endpoint(addr).unwrap();
+    // accept a single connection
+    let incoming_conn = endpoint.accept().await.unwrap();
+    let conn = incoming_conn.await.unwrap();
+
+    // open_bidirectional_stream(conn.clone()).await;
+
+
+    println!(
+        "[server] connection accepted: addr={}",
+        conn.remote_address()
+    );
+
+    let mut send = conn.open_uni().await?;
+
+    send.write_all(b"test").await?;
+    send.finish().await?;
+
+    Ok(())
+
+}
+
+// async fn open_bidirectional_stream(connection: Connection) -> Result<(), dyn Error> {
+//     let (mut send, mut recv) = connection
+//         .open_bi()
+//         .await?;
+//
+//     send.write_all(b"test").await?;
+//     send.finish().await?;
+//
+//     let received = recv.read_to_end(10).await?;
+//
+//     Ok(())
+// }
+
+
+pub(crate) async fn run_client(server_addr: SocketAddr) -> Result<(), Box<dyn Error>> {
+    let mut endpoint = Endpoint::client("127.0.0.1:0".parse().unwrap())?;
+    endpoint.set_default_client_config(configure_client());
+
+    // connect to server
+    let connection = endpoint
+        .connect(server_addr, "localhost")
+        .unwrap()
+        .await
+        .unwrap();
+
+    // receive_bidirectional_stream(connection.clone()).await;
+
+    println!("[client] connected: addr={}", connection.remote_address());
+    // Dropping handles allows the corresponding objects to automatically shut down
+
+    while let Ok(mut recv) = connection.accept_uni().await {
+        // Because it is a unidirectional stream, we can only receive not send back.
+        println!("{:?}", recv.read_to_end(50).await?);
+    }
+
+    drop(connection);
+    // Make sure the server has a chance to clean up
+    endpoint.wait_idle().await;
+
+    Ok(())
+}
+
+// async fn receive_bidirectional_stream(connection: Connection) -> Result<(), dyn Error> {
+//     while let Ok((mut send, mut recv)) = connection.accept_bi().await {
+//         // Because it is a bidirectional stream, we can both send and receive.
+//         println!("request: {:?}", recv.read_to_end(50).await?);
+//
+//         send.write_all(b"response").await?;
+//         send.finish().await?;
+//     }
+//
+//     Ok(())
+// }
+
+
+
+
+/// Dummy certificate verifier that treats any certificate as valid.
+/// NOTE, such verification is vulnerable to MITM attacks, but convenient for testing.
+struct SkipServerVerification;
+
+impl SkipServerVerification {
+    fn new() -> Arc<Self> {
+        Arc::new(Self)
+    }
+}
+
+impl rustls::client::ServerCertVerifier for SkipServerVerification {
+    fn verify_server_cert(
+        &self,
+        _end_entity: &rustls::Certificate,
+        _intermediates: &[rustls::Certificate],
+        _server_name: &rustls::ServerName,
+        _scts: &mut dyn Iterator<Item = &[u8]>,
+        _ocsp_response: &[u8],
+        _now: std::time::SystemTime,
+    ) -> Result<rustls::client::ServerCertVerified, rustls::Error> {
+        Ok(rustls::client::ServerCertVerified::assertion())
+    }
+}
+
+fn configure_client() -> ClientConfig {
+    let crypto = rustls::ClientConfig::builder()
+        .with_safe_defaults()
+        .with_custom_certificate_verifier(SkipServerVerification::new())
+        .with_no_client_auth();
+
+    ClientConfig::new(Arc::new(crypto))
+}
